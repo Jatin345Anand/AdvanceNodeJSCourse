e215cd633c8acf1a03d5c0d0ffe8e7911a06f8e1
Jatin Anand <jatin345anand@gmail.com>
2020-07-22 00:37:19 +0530

    100% Section 8 are done.

--- Section-7-8_Introduction_to_MongoDB_Using_MongoDB_with_Mongoose/routes/imageRoutes.js
+++ Section-7-8_Introduction_to_MongoDB_Using_MongoDB_with_Mongoose/routes/imageRoutes.js
@@ -3,6 +3,7 @@ const imageRouter = express.Router();
 const imageController = require('./../controllers/imageController');
 imageRouter.route('/top-5-cheap').get(imageController.aliasTopImages, imageController.getAllImages);
 imageRouter.route('/image-stats').get(imageController.getImagesStats);
+imageRouter.route('/image-monthy-plan/:year').get(imageController.getMonthlyPlan);
 imageRouter.route('/').get(imageController.getAllImages).post(imageController.addImage);
 imageRouter.route('/:id').get(imageController.getImagebyId).patch(imageController.updateAImage).delete(imageController.deleteImage);
 module.exports = imageRouter;
